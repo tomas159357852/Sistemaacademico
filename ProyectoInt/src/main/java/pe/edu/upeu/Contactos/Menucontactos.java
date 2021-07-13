d64b3acb2b49c1549194abf8dd72cf7104136aef
tomas159357852 <george159357852@gmail.com>
2021-07-13 18:35:11 -0500

    hola

--- ProyectoInt/src/main/java/pe/edu/upeu/Contactos/Menucontactos.java
+++ ProyectoInt/src/main/java/pe/edu/upeu/Contactos/Menucontactos.java
@@ -9,6 +9,23 @@ import pe.edu.upeu.Datos.Usuarios;
 public class Menucontactos{
     LeerTeclado leer=new LeerTeclado();
     
+    public void Matematicas() {
+        
+    }
+
+    public void Ingles() {
+        
+    }
+
+    public void programacion() {
+
+    }
+
+    public void estadistica() {
+        
+    }
+
+
     public void Menuprincipal() {
         System.out.println("|---Bievenido al sistema de ayuda de estudiantes.---|"
         +"\n Encuentrar ayuda con:"
@@ -19,9 +36,23 @@ public class Menucontactos{
         +"\n ------------------------"
         +"\n 5.- Conocer más sobre la aplicación"
         +"\n 6.- Deseo apoyar esta app");
-    public void Matematicas(){
-        
-    }
+    int opcionesc=leer.leer(0,"Ingresa la opción correspondiente");
+    do{
+        switch(opcionesc){
+            case 1: Matematicas();
+            break;
+            case 2: Ingles();
+            break;
+            case 3: programacion();
+            break;
+            case 4: estadistica();
+            break;
+            case 5:
+            break;
+            case 6:
+            break;
+        }
+    }while(opcionesc!=0);
         
     }
 
